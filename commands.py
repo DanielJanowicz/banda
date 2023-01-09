5ff012ab14850d658a4c7210ff1d223817c79b4e
DanielJanowicz <102616987+DanielJanowicz@users.noreply.github.com>
2023-01-08 19:25:45 -0500

    Foundation
    Laid out basic foundation

---
+++ commands.py
@@ -0,0 +1,14 @@
+import random
+
+def get_command(message: str) -> str:
+    p_command = message.lower()
+
+    if p_command == '$hello':
+        return 'Hey there!'
+    
+    if p_command == '$roll':
+        return str(random.randint(1, 9))
+
+    if p_command == '!help':
+        return '`It looks like you need help!  This function is currently a work in progress.`'
+    
